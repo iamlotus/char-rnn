fbcb444668c346c387a46997dbd97169451b7b04
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-26 21:26:52 +0800

    modified

--- train_shakespeare.sh
+++ train_shakespeare.sh
@@ -1,1 +1,1 @@
-mkdir -p logs && nohup th train.lua -data_dir data/tinyshapespeare -rnn_size 128 -num_layers 2 -dropout 0.5 -max_epochs 500 -print_every 100 -eval_val_every 1000 -savefile shakespeare -gpuid 1 >logs/shakespeare.out 2>&1 & busybox tail -f logs/shakespeare.out
+mkdir -p logs && nohup th train.lua -data_dir data/tinyshakespeare -rnn_size 128 -num_layers 2 -dropout 0.5 -max_epochs 500 -print_every 100 -eval_val_every 1000 -savefile shakespeare -gpuid 1 >logs/shakespeare.out 2>&1 & busybox tail -f logs/shakespeare.out
